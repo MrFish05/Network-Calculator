4f7466a472aa0f8e775b2c26468e4e206142d416
Kristian Tomasko <kristian.tomasko.2105@gmail.com>
2025-10-19 17:44:18 +0200

    Setup: decimal to binary

---
+++ ip_calculator.py
@@ -0,0 +1,40 @@
+# Converts a decimal number into binary
+def decimal_to_binary(decimal_number):
+    number = int(decimal_number)
+    res = ""
+
+    while number != 0: 
+        if number%2 != 0:
+            res = "1"+res
+        else:
+            res = "0"+res
+
+        number = int(number/2)
+
+    return res
+
+# Converts a binary number in 8 bit lenght if smaller
+def eight_bit_binary(binary_number):
+    res = binary_number
+
+    while(len(res) < 8):
+        res = "0"+res
+
+    return res
+        
+
+# Converts a decimal ip address in a binary ip address
+def decimal_ip_to_binary_ip(decimal_ip):
+    ip = decimal_ip.split(".")
+    res = ""
+    x = 0
+
+    for i in ip:
+        if x != 3:
+            res = res+eight_bit_binary(decimal_to_binary(i))+"."
+            x = x+1
+        else:
+            res = res+eight_bit_binary(decimal_to_binary(i))
+            x = x+1
+
+    return res
